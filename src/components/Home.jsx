443d954f37dd35674a3eec468285a89fd7ab5ef5
unknown <chadiegilaugis@gmail.com>
2022-10-01 14:37:20 +0800

    feature - input search

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
-
 import Card from "./card/Card";
+import { CgSearch } from "react-icons/cg";
 
 import "./index.css";
 
 const Home = () => {
   const [post, setPost] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  const [inputSearch, setInputSearch] = useState("");
 
   const getItems = () => {
     return fetch("http://localhost:3500/posts")
@@ -25,10 +27,29 @@ const Home = () => {
   return (
     <>
       {!loading && <h1 className="text-center loading">loading..</h1>}
+
+      <div className="container d-flex align-items-center justify-content-center">
+        <input
+          type="text"
+          name="search"
+          placeholder="search name..."
+          className="search"
+          onChange={(e) => setInputSearch(e.target.value)}
+        />
+      </div>
       {post &&
-        post.map((post) => {
-          return <Card post={post} />;
-        })}
+        post
+          .filter((value) => {
+            if (inputSearch == "") return value;
+            if (
+              value.name.toLowerCase().includes(inputSearch.toLocaleLowerCase())
+            ) {
+              return value;
+            }
+          })
+          .map((post) => {
+            return <Card post={post} />;
+          })}
     </>
   );
 };
